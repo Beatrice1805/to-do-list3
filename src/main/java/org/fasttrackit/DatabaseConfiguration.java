0fbd8fbc7711bc083e55f8dafb013331f470b023
Beatrice <oprea.beatrice@yahoo.com>
2020-01-15 20:37:28 +0200

    added database configuration

---
+++ src/main/java/org/fasttrackit/DatabaseConfiguration.java
@@ -0,0 +1,36 @@
+package org.fasttrackit;
+
+import org.graalvm.compiler.hotspot.nodes.CurrentLockNode;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConfiguration {
+
+    public static Connection getConnection() throws IOException, SQLException {
+        InputStream dbProperties = DatabaseConfiguration.class.getClassLoader()
+                .getResourceAsStream("db.properties");
+
+        try {
+            Properties properties = new Properties();
+            properties.load(dbProperties);
+
+            return DriverManager.getConnection(
+                    properties.getProperty("url"),
+                    properties.getProperty("username"),
+                    properties.getProperty("password"));
+        } finally {
+            //closing input stream to allow memory clean up
+            if (dbProperties != null) {
+                dbProperties.close();
+            }
+        }
+
+    }
+
+
+    }
